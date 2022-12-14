874bb8d656bf5845b6eb6cab19f46c67a614c157
yongjer <jerjer920813@gmail.com>
2022-12-14 18:14:57 +0800

    atoi

--- main.c
+++ main.c
@@ -6,8 +6,9 @@ int main(void)
 {
     char str[81];
     fgets(str, 80, stdin);
-    char num[81];
-    fgets(num, 80, stdin);
+    char nums[81];
+    int num[81][81];
+    fgets(nums, 80, stdin);
     printf("%s", str);
     // separate the string into an array of chars
     char *token = strtok(str, " ");
@@ -19,3 +20,11 @@ int main(void)
         i++;
     }
     // convert the string to an int
+    int n = atoi(num);
+    num_counter = 0;
+    for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {
+            num[j][k] = atoi(arr[num_counter]);
+            num_counter++;
+        }
+    }
