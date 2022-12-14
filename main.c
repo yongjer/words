98e045fa83d5ef8a69d726357866c879c3403e1e
yongjer <jerjer920813@gmail.com>
2022-12-14 13:44:09 +0800

    memory shortage

--- main.c
+++ main.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main(void)
 {
-    char str[80];
+    char str[81];
     fgets(str, 80, stdin);
-    char num[80];
+    char num[81];
     fgets(num, 80, stdin);
     printf("%s", str);
     for (int i = 0; i < 80; i++)
-    {
-        
-    }
